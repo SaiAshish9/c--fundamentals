e5ac5eebd44b01f1318aa560bb3d9c198573ecb0
saiashish9 <saiashish3760@gmail.com>
2021-02-05 19:01:31 +0530

    exceptions updated

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -12,14 +12,40 @@ namespace hello_world
             //     x+=1;
             // }
             char x = 'x';
-            string y ="xy";
+            string y = "xy";
             var v = 'x';
             const float c = 1;
-            Console.WriteLine("{0} {1}",x,c);
+            Console.WriteLine("{0} {1}", x, c);
             Console.WriteLine(y);
             Console.WriteLine(v);
             Console.WriteLine("Hello World!");
-            Console.WriteLine("{0} {1}",int.MinValue,int.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+
+
+            string s = "1";
+            int i = Convert.ToInt32(s);
+            // ToInt16 ToByte ToInt64 ToBoolean
+            int j = int.Parse(s);
+            // int 32 -> int 8*4
+            Console.WriteLine("{0} {1} {2}", s, i, j);
+
+            try
+            {
+                var num = "a121";
+                byte b = Convert.ToByte(num);
+                // string str = "true";
+                // bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("number could not be converted to a byte");
+            }
+
         }
+
+
+
+
     }
 }
