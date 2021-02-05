8b47ceacd46e8c7a7e1ed695e38a5045d23dfab5
saiashish9 <saiashish3760@gmail.com>
2021-02-05 22:45:18 +0530

    enums and strings updated

--- operators/Program.cs
+++ operators/Program.cs
@@ -8,6 +8,11 @@ namespace operators
         {
             var a = 10;
             var b = 3;
+
+            var x = new bool[3]{true,false,true};
+            int[] y=new int[3];
+            y[0]=4;
+            Console.WriteLine("{0}",y[0],y[2]);
             Console.WriteLine((float)a / (float)b);
             Console.WriteLine("{0} {1} {2}", a == b, a != b, a == b);
             Console.WriteLine(a > b || a == b);
---
+++ strings-enum-reference-types/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace strings_enum_reference_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "abc";
+            Console.WriteLine(string.Format("{0}", x));
+            char c = x[0];
+            Console.WriteLine(c);
+            var num = new int[3] { 1, 2, 3 };
+            Console.WriteLine(string.Join(",", num));
+
+            // strings are immutable
+
+            string path = "c:\\projects\\proect1\\folder1";
+            Console.WriteLine(path);
+            Console.WriteLine("{0} {1}", A.b, (int)A.b);
+            var d = 1;
+            Console.WriteLine((A)d);
+            Console.WriteLine(d.ToString());
+            Console.WriteLine((A)Enum.Parse(typeof(A),"a"));
+        }
+        public enum A
+        {
+            a = 1,
+            b = 2,
+            c = 3
+        }
+
+
+
+
+    }
+}
