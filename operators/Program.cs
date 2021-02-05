322d615efa22e5129b2ae3c434a2d1e70810e8d0
saiashish9 <saiashish3760@gmail.com>
2021-02-05 19:23:06 +0530

    operators updated

---
+++ operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine("{0} {1} {2}",a==b,a!=b,a==b);
+            Console.WriteLine(a>b || a==b);
+            Console.WriteLine(a|b);
+
+
+
+
+        }
+    }
+}
