c94d206f3812f591b22a3b58351c49d7214def39
saiashish9 <saiashish3760@gmail.com>
2021-02-05 23:52:59 +0530

    reference types updated

--- value-types-reference-types/Program.cs
+++ value-types-reference-types/Program.cs
@@ -2,11 +2,34 @@
 
 namespace value_types_reference_types
 {
+    public class Person{
+        public int Age;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var p  = new Person(){
+                Age = 20
+            };
+
+            MakeOld(p);
+
+            Console.WriteLine(p.Age);
+
         }
+
+        public static void Increment(int number){
+            number += 10;
+        }
+
+        public static void MakeOld(Person p){
+            p.Age += 10;
+        }  
+
     }
 }
