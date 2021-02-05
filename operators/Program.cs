8d5a638fc2317cba29d111da80dc69c0421849bc
saiashish9 <saiashish3760@gmail.com>
2021-02-05 19:31:55 +0530

    operators updated

--- operators/Program.cs
+++ operators/Program.cs
@@ -9,13 +9,12 @@ namespace operators
             var a = 10;
             var b = 3;
             Console.WriteLine((float)a / (float)b);
-            Console.WriteLine("{0} {1} {2}",a==b,a!=b,a==b);
-            Console.WriteLine(a>b || a==b);
-            Console.WriteLine(a|b);
-
-
-
-
+            Console.WriteLine("{0} {1} {2}", a == b, a != b, a == b);
+            Console.WriteLine(a > b || a == b);
+            Console.WriteLine(a | b);
+            // 
+            /* a=10;
+            a=11; */
         }
     }
 }
