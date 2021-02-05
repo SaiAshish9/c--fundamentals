dbdfe20979e4e1039a4e9960a1d9d62f061d0447
saiashish9 <saiashish3760@gmail.com>
2021-02-05 18:37:39 +0530

    overflowing, datatypes and scope updated

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,7 +6,20 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
+            // decimal a = 1.2m;
+            // checked{
+            //     byte x =255;
+            //     x+=1;
+            // }
+            char x = 'x';
+            string y ="xy";
+            var v = 'x';
+            const float c = 1;
+            Console.WriteLine("{0} {1}",x,c);
+            Console.WriteLine(y);
+            Console.WriteLine(v);
             Console.WriteLine("Hello World!");
+            Console.WriteLine("{0} {1}",int.MinValue,int.MaxValue);
         }
     }
 }
