046bcb61e2f2c30db7cca6514038a74a38020725
saiashish9 <saiashish3760@gmail.com>
2021-02-06 19:12:35 +0530

    directories and path updated

---
+++ directories/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"obj1");
+            var files = Directory.GetFiles(@"obj", "*.*", SearchOption.AllDirectories);
+            var directories = Directory.GetDirectories(@"obj", "*.*", SearchOption.AllDirectories);
+            foreach (var i in files)
+            {
+                System.Console.WriteLine(i);
+            }
+
+            // Directory.Exists("*.*)
+            // new DirectoryInfo("...").GetFiles()/.GetDirectories()
+
+            var path = @"C:\Projects\a.sln";
+
+            System.Console.WriteLine(path.IndexOf("."));
+            System.Console.WriteLine(
+                path.Substring(path.IndexOf("."))
+            );
+            System.Console.WriteLine(Path.GetExtension(path));
+            System.Console.WriteLine(
+                Path.GetFileName(path)
+            );
+            System.Console.WriteLine(
+                Path.GetFileNameWithoutExtension(path)
+            );
+            System.Console.WriteLine(
+                Path.GetDirectoryName(path)
+            );
+
+
+        }
+    }
+}
