aacfbbc7c3c76ff841d1566cd58de90b6d40adb0
saiashish9 <saiashish3760@gmail.com>
2021-02-06 17:23:23 +0530

    stringbuilder updated

---
+++ stringbuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // mutable string
+            // Append Insert Remove Replace Clear  
+
+            var builder = new StringBuilder("hi");
+            builder.Append('-', 10)
+            .AppendLine()
+            .Append("hi");
+            System.Console.WriteLine(builder);
+            builder.Replace('-', '+')
+            .Remove(2, 13);
+            System.Console.WriteLine(builder);
+            builder.Insert(0, new string('-', 10));
+            System.Console.WriteLine(builder);
+        }
+    }
+}
